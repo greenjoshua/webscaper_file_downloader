96e22421b4493be4f22e458afd4da04e8a9b24ae
Joshua Green <joshuag331@gmail.com>
2019-10-03 01:08:30 +0000

    Modified download_csv function to accept a file_name parameter for ease of naming files

--- Test_Folder/download_files.py
+++ Test_Folder/download_files.py
@@ -1,9 +1,9 @@
 import csv
 import requests
 
-def download_csv(file_name):
+def download_csv(link_name, file_name):
     with requests.Session() as s:
-        download = s.get(file_name)
+        download = s.get(link_name)
         decoded_content = download.content.decode('utf-8')
-    file = open('balls.csv', 'wt')
+    file = open(file_name + ".csv", 'wt')
     file.write(decoded_content)
--- Test_Folder/rd2.py
+++ Test_Folder/rd2.py
@@ -13,15 +13,17 @@ match = soup.find('body')
 match_h2 = match.find_all("h2")
 
 tagList = [str(tag) for tag in match_h2]
+trial_file_names = []
 preLink = []
 for x in range(len(tagList)):
   trial = tagList[x]
   trial1 = trial.split('href="/replay/')[1].split('">')[0]
   """ Josh did this line. See Josh for changes cuz he killed it."""
+  trial_file_names.append(trial1)
   trial1 = "http://www.ballchasing.com/dl/stats/players/" + trial1 + "/" + trial1 + "-players.csv"
   preLink.append(trial1)
  
-download_files.download_csv(preLink[0])
+download_files.download_csv(preLink[0], trial_file_names[0])
 
 
 #print(preLink)
